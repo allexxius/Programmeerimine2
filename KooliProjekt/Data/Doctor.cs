66b9f58a5be67e972f4a3380758d9da358613e7b
Nikita Guljajev <Nikita Guljajev>
2024-11-08 11:22:40 +0200

    commit docaddn

--- KooliProjekt/Data/Doctor.cs
+++ KooliProjekt/Data/Doctor.cs
@@ -10,8 +10,13 @@ namespace KooliProjekt.Data
         [Required]
         [StringLength(50)]
         public string Specialization { get; set; }
+        
+        [StringLength(50)]
+        public string Name { get; set; }
 
         [Required]
         public int UserId { get; set; }
+
+
     }
 }
