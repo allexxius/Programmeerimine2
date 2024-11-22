5d7293e237867bb228ed312d87aba19fcdaa7585
Nikita Guljajev <Nikita Guljajev>
2024-11-22 11:29:03 +0200

    nknk some changes

---
+++ KooliProjekt/Data/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace KooliProjekt.Data
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+    }
+}
--- KooliProjekt/Services/DoctorService.cs
+++ KooliProjekt/Services/DoctorService.cs
@@ -1,3 +1,53 @@
-﻿
+﻿using KooliProjekt.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KooliProjekt.Services
+{
+    public class DoctorService : IDoctorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DoctorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PagedResult<Doctor>> List(int page, int pageSize)
+        {
+            return await _context.Doctors.GetPagedAsync(page, 5);
+        }
+
+        public async Task<Doctor> Get(int id)
+        {
+            return await _context.Doctors.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task Save(Doctor list)
+        {
+            if (list.Id == 0)
+            {
+                _context.Add(list);
+            }
+            else
+            {
+                _context.Update(list);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var todoList = await _context.Doctors.FindAsync(id);
+            if (todoList != null)
+            {
+                _context.Doctors.Remove(todoList);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
+
 
 
