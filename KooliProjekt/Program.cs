3aab86362d544eddcb73d6c2224c4cb66fdec49b
Aleks Nazarenko <nazaren.aleks@gmail.com>
2025-01-24 08:21:56 +0200

    d

--- KooliProjekt/Program.cs
+++ KooliProjekt/Program.cs
@@ -11,8 +11,9 @@ namespace KooliProjekt
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+           
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -21,14 +22,13 @@ namespace KooliProjekt
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
-            builder.Services.AddScoped<ApplicationDbContext>();
-
+          
             builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+        
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -36,7 +36,6 @@ namespace KooliProjekt
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -53,11 +52,17 @@ namespace KooliProjekt
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
+
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 var dbContext = services.GetRequiredService<ApplicationDbContext>();
-                SeedData.Generate(dbContext); // Call the SeedData method
+
+                if (app.Environment.IsDevelopment())
+                {
+                    dbContext.Database.Migrate();
+                    SeedData.Generate(dbContext);
+                }
             }
 
             app.Run();
