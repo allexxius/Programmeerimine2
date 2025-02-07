d9864af24d649a9c04dd2b5678f2f2ed91dbb657
Aleks Nazarenko <nazaren.aleks@gmail.com>
2025-02-07 10:33:42 +0200

    added service tests

---
+++ KooliProjekt.UnitTests/ServiceTests/TimeServiceTests.cs
@@ -0,0 +1,180 @@
+﻿using KooliProjekt.Data;
+
+using KooliProjekt.Services;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace KooliProjekt.UnitTests.ServiceTests
+
+{
+
+    public class TimeServiceTests : ServiceTestBase
+
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        private readonly TimeService _timeService;
+
+        public TimeServiceTests()
+
+        {
+
+            _context = DbContext;
+
+            _timeService = new TimeService(_context);
+
+            // Add test data
+
+            _context.Times.AddRange(new List<Time>
+
+            {
+
+                new Time { Id = 1, Date = DateTime.Now, VisitTime = new TimeOnly(10, 0), Free = true, DoctorId = 1 },
+
+                new Time { Id = 2, Date = DateTime.Now, VisitTime = new TimeOnly(11, 0), Free = false, DoctorId = 2 },
+
+                new Time { Id = 3, Date = DateTime.Now, VisitTime = new TimeOnly(12, 0), Free = true, DoctorId = 3 }
+
+            });
+
+            _context.SaveChanges();
+
+        }
+
+        [Fact]
+
+        public async Task List_WithoutFilters_ReturnsAllTimes()
+
+        {
+
+            var page = 1;
+
+            var pageSize = 10;
+
+            var result = await _timeService.List(page, pageSize);
+
+            Assert.Equal(3, result.Results.Count);
+
+        }
+
+        [Fact]
+
+        public async Task Get_ExistingTimeId_ReturnsTime()
+
+        {
+
+            var timeId = 3;
+
+            var result = await _timeService.Get(timeId);
+
+            Assert.NotNull(result);
+
+            Assert.Equal(12, result.VisitTime.Hour);
+
+        }
+
+        [Fact]
+
+        public async Task Get_NonExistingTimeId_ReturnsNull()
+
+        {
+
+            var timeId = 999;
+
+            var result = await _timeService.Get(timeId);
+
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+
+        public async Task Save_NewTime_AddsTimeToDatabase()
+
+        {
+
+            var newTime = new Time { Date = DateTime.Now, VisitTime = new TimeOnly(13, 0), Free = true, DoctorId = 4 };
+
+            await _timeService.Save(newTime);
+
+            await _context.SaveChangesAsync();
+
+            var result = await _context.Times.FindAsync(newTime.Id);
+
+            Assert.NotNull(result);
+
+            Assert.Equal(13, result.VisitTime.Hour);
+
+        }
+
+        [Fact]
+
+        public async Task Save_ExistingTime_UpdatesTimeInDatabase()
+
+        {
+
+            var existingTime = await _context.Times.FindAsync(1);
+
+            existingTime.Free = false;
+
+            await _timeService.Save(existingTime);
+
+            await _context.SaveChangesAsync();
+
+            var result = await _context.Times.FindAsync(1);
+
+            Assert.NotNull(result);
+
+            Assert.False(result.Free);
+
+        }
+
+        [Fact]
+
+        public async Task Delete_ExistingTimeId_RemovesTimeFromDatabase()
+
+        {
+
+            var timeId = 2;
+
+            await _timeService.Delete(timeId);
+
+            await _context.SaveChangesAsync();
+
+            var result = await _context.Times.FindAsync(timeId);
+
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+
+        public async Task Delete_NonExistingTimeId_DoesNothing()
+
+        {
+
+            var timeId = 999;
+
+            await _timeService.Delete(timeId);
+
+            await _context.SaveChangesAsync();
+
+            var result = await _context.Times.FindAsync(timeId);
+
+            Assert.Null(result);
+
+        }
+
+    }
+
+}
+
---
+++ KooliProjekt.UnitTests/ServiceTests/VisitServiceTests.cs
@@ -0,0 +1,180 @@
+﻿using KooliProjekt.Data;
+
+using KooliProjekt.Services;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace KooliProjekt.UnitTests.ServiceTests
+
+{
+
+    public class VisitServiceTests : ServiceTestBase
+
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        private readonly VisitService _visitService;
+
+        public VisitServiceTests()
+
+        {
+
+            _context = DbContext;
+
+            _visitService = new VisitService(_context);
+
+            // Add test data
+
+            _context.Visits.AddRange(new List<Visit>
+
+            {
+
+                new Visit { Id = 1, Name = "Visit A", UserId = "user1", DoctorId = 1, Date = DateTime.Now, Duration = 30 },
+
+                new Visit { Id = 2, Name = "Visit B", UserId = "user2", DoctorId = 2, Date = DateTime.Now, Duration = 45 },
+
+                new Visit { Id = 3, Name = "Visit C", UserId = "user3", DoctorId = 3, Date = DateTime.Now, Duration = 60 }
+
+            });
+
+            _context.SaveChanges();
+
+        }
+
+        [Fact]
+
+        public async Task List_WithoutFilters_ReturnsAllVisits()
+
+        {
+
+            var page = 1;
+
+            var pageSize = 10;
+
+            var result = await _visitService.List(page, pageSize);
+
+            Assert.Equal(3, result.Results.Count);
+
+        }
+
+        [Fact]
+
+        public async Task Get_ExistingVisitId_ReturnsVisit()
+
+        {
+
+            var visitId = 2;
+
+            var result = await _visitService.Get(visitId);
+
+            Assert.NotNull(result);
+
+            Assert.Equal("Visit B", result.Name);
+
+        }
+
+        [Fact]
+
+        public async Task Get_NonExistingVisitId_ReturnsNull()
+
+        {
+
+            var visitId = 999;
+
+            var result = await _visitService.Get(visitId);
+
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+
+        public async Task Save_NewVisit_AddsVisitToDatabase()
+
+        {
+
+            var newVisit = new Visit { Name = "Visit D", UserId = "user4", DoctorId = 4, Date = DateTime.Now, Duration = 30 };
+
+            await _visitService.Save(newVisit);
+
+            await _context.SaveChangesAsync();
+
+            var result = await _context.Visits.FindAsync(newVisit.Id);
+
+            Assert.NotNull(result);
+
+            Assert.Equal("Visit D", result.Name);
+
+        }
+
+        [Fact]
+
+        public async Task Save_ExistingVisit_UpdatesVisitInDatabase()
+
+        {
+
+            var existingVisit = await _context.Visits.FindAsync(1);
+
+            existingVisit.Duration = 90;
+
+            await _visitService.Save(existingVisit);
+
+            await _context.SaveChangesAsync();
+
+            var result = await _context.Visits.FindAsync(1);
+
+            Assert.NotNull(result);
+
+            Assert.Equal(90, result.Duration);
+
+        }
+
+        [Fact]
+
+        public async Task Delete_ExistingVisitId_RemovesVisitFromDatabase()
+
+        {
+
+            var visitId = 3;
+
+            await _visitService.Delete(visitId);
+
+            await _context.SaveChangesAsync();
+
+            var result = await _context.Visits.FindAsync(visitId);
+
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+
+        public async Task Delete_NonExistingVisitId_DoesNothing()
+
+        {
+
+            var visitId = 999;
+
+            await _visitService.Delete(visitId);
+
+            await _context.SaveChangesAsync();
+
+            var result = await _context.Visits.FindAsync(visitId);
+
+            Assert.Null(result);
+
+        }
+
+    }
+
+}
+
